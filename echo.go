d45059cb7bab0f85c2388c68b6d83623b429f834
Frank Cash <cfcash@mtu.edu>
2017-06-03 09:28:48 -0400

    Init

---
+++ echo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main(){
+
+	s, sep := "",""
+	for index, arg := range os.Args[1:]{
+		s += sep + arg
+		sep = " "
+		fmt.Println(index)
+		fmt.Println(arg)
+	}	
+	fmt.Println("Command" + os.Args[0])
+	fmt.Println(s)
+
+ fmt.Println(strings.Join(os.Args[1:], " "))
+}
---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, world")
+	var(
+		name = "r00ty"
+		age = 23
+	)
+
+	location := "Blahastan"
+
+	fmt.Printf("%s (%d) of %s", name, age, location)
+}
