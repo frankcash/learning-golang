51dc9e08b6407451797bdd679dc177c40def87c4
Frank Cash <cfcash@mtu.edu>
2017-06-03 09:46:00 -0400

    Dup v2

---
+++ dup.go
@@ -0,0 +1,44 @@
+// Prints lines that appear more than once
+
+package main
+
+import(
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main(){
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0{
+		countLines(os.Stdin, counts)
+	}else{
+		for _, args := range files{
+			f, err := os.Open(args)
+			if(err != nil){
+				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+				continue
+			}
+
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts{
+		if n > 1{
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+
+}
+
+func countLines(f *os.File, counts map[string]int){
+	input := bufio.NewScanner(f)
+
+	for input.Scan(){
+		counts[input.Text()]++
+	}
+	// NOTE: Ignoring err from input.Err()
+}
