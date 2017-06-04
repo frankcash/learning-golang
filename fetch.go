6e4a55e5baed36a1f546edace2debf5e76e1a4fd
Frank Cash <cfcash@mtu.edu>
2017-06-04 15:35:58 -0400

    Checks for HTTP or HTTPS

--- fetch.go
+++ fetch.go
@@ -7,11 +7,15 @@ import(
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
 )
 
 func main(){
 	for _, url := range os.Args[1:]{
+		if !strings.HasPrefix(url, "http://") || !strings.HasPrefix(url, "https://"){
+			url = "http://" + url
+		}	
 		resp, err := http.Get(url)
 		if err != nil{
 			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
