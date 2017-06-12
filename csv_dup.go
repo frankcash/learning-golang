f3e24d9b1a050f174309c6404ca6ac1da48b3764
Frank Cash <cashc@acm.org>
2017-06-12 11:17:26 -0400

    Adds dynamic column searching incase non-standarized

--- csv_dup.go
+++ csv_dup.go
@@ -5,24 +5,40 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main(){
-	file, err := os.Open("test.csv")
+	for _, tempCsv := range os.Args[1:]{
+		readCsv(tempCsv, "condition") // starts a go routine
+	}
+}
+
+func readCsv(myCsv string, desiredCondition string	){
+	var colIndex int
+
+	fmt.Printf("Running %s\n", myCsv)
+	file, err := os.Open(myCsv)
 	if err != nil{
 		fmt.Println("Err ", err)
 		return
 	}
 
 	defer file.Close()
-
 	reader := csv.NewReader(file)
+	first_row, _ := reader.Read()
+	for i, col := range first_row{
+		if strings.EqualFold(desiredCondition, col) {
+			colIndex = i
+		}
+	}
 
+	fmt.Println(colIndex)
 
 	lineCount := 0
 	counts := make(map[string]int)
 	for{
-		record, err := reader.Read()	
+		record, err := reader.Read()
 		if err == io.EOF{
 			break
 		}else if err != nil {
@@ -30,19 +46,16 @@ func main(){
 			return
 		}
 
-		// fmt.Println("Record", lineCount, " is ", record, " and has ", len(record), "fields")
 
-		counts[record[2]]++
+		counts[record[colIndex]]++
 
-		// fmt.Println()
 		lineCount += 1
 	}
-	
+
 	for line, n := range counts{
-		if n > 1{
+		if n > -1{
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 
 }
-
