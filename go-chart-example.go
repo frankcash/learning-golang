c0fabccd48ae65fd4819a89ced06fdd6dfd8cc7e
Frank Cash <cashc@acm.org>
2017-07-13 16:30:45 -0400

    Simple example of charting in golang

---
+++ go-chart-example.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/wcharczuk/go-chart"
+	"log"
+	"net/http"
+)
+
+func drawChart(res http.ResponseWriter, req *http.Request) {
+	graph := chart.Chart{
+		Series: []chart.Series{
+			chart.ContinuousSeries{
+				XValues: []float64{1.0, 2.0, 3.0, 4.0},
+				YValues: []float64{1.0, 2.0, 3.0, 4.0},
+			},
+		},
+	}
+	res.Header().Set("Content-Type", "image/png")
+	graph.Render(chart.PNG, res)
+}
+
+func main() {
+	http.HandleFunc("/", drawChart)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
